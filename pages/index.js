5a536484e7890b65ad63a36a22cfaf0e4fef778e
brigitamaria <brigitamariawiputri@brigitamaria.local>
2020-04-19 20:36:17 +0700

    feat: integrate pokedex

--- pages/index.js
+++ pages/index.js
@@ -1,204 +1,109 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { useState, useEffect } from "react";
 
-export default function Home() {
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Grid from "@material-ui/core/Grid";
+import Header from "../components/Header";
+import PokemonCardsContainer from "../components/pokemon/PokemonCardsContainer";
+import PokemonDetail from "../components/pokemon/PokemonDetail/PokemonDetail";
+import { fetchNext, initialFetch } from "../datasource/action";
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    maxWidth: "50%",
+    margin: "auto",
+  },
+}));
+
+function Home({ pokemons, next }) {
+  const classes = useStyles();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [selectedFilter, setSelectedFilter] = useState(null);
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
+  const [renderedPokemons, setRenderedPokemons] = useState(pokemons);
+  const [renderedNext, setRenderedNext] = useState(next);
+
+  const handleCloseFilter = (value, event) => {
+    if (!value && selectedFilter) {
+      setSelectedFilter(selectedFilter);
+    } else setSelectedFilter(value);
+    setAnchorEl(null);
+  };
+  const handleClickFilter = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleCloseModal = () => {
+    setSelectedPokemon(null);
+  };
+  const handleScroll = async () => {
+    if (renderedNext && window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
+      await fetchNext(renderedNext, renderedPokemons, setRenderedPokemons, setRenderedNext);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  });
+
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Pokédex</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <Header
+          handleClose={handleCloseFilter}
+          handleClick={handleClickFilter}
+          selectedFilter={selectedFilter}
+          setSelectedFilter={setSelectedFilter}
+          anchorEl={anchorEl}
+        />
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with ZEIT Now.
-            </p>
-          </a>
+        <div className="pokedexContainer">
+          <Grid container spacing={4}>
+            <PokemonCardsContainer pokemons={renderedPokemons} selectedFilter={selectedFilter} setSelectedPokemon={setSelectedPokemon} />
+            {selectedPokemon && (
+              <Modal className={classes.modal} open={true} onClose={handleCloseModal}>
+                <PokemonDetail selectedPokemon={selectedPokemon} />
+              </Modal>
+            )}
+          </Grid>
         </div>
       </main>
-
-      <footer>
-        <a
-          href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
 
       <style jsx global>{`
         html,
         body {
           padding: 0;
           margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
         }
 
         * {
           box-sizing: border-box;
         }
+
+        .pokedexContainer {
+          padding: 64px;
+          flex-grow: 1;
+        }
       `}</style>
     </div>
-  )
+  );
 }
+
+export async function getStaticProps() {
+  const { pokemons, next } = await initialFetch();
+  return {
+    props: { pokemons, next },
+  };
+}
+
+export default Home;
